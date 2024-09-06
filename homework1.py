db96d6bfcd0a0fcb765a7a2fd82eb92ab24bc018
UASTRONGMAN <shkolniy.maks@ukr.net>
2024-09-06 20:05:23 +0300

    hw1

--- homework1.py
+++ homework1.py
@@ -106,96 +106,72 @@ from operator import contains
 
 
 # 1)Дан list:
-# l = [22, 3,5,2,8,2,-23, 8,23,5]
+l = [22, 3,5,2,8,2,-23, 8,23,5]
 #   - знайти мін число
-# def min_list_num(l):
-#     return min(l)
+def min_list_num(l):
+    print(min(l))
 # min_list_num(l)
 
 #   - видалити усі дублікати
 
-# set = set(l)
-# print(set)
+def set_list(l):
+    copy = l.copy()
+    print(set(copy))
+# set_list(l)
 
 #   - замінити кожне 4-те значення на 'X'
 
-# def change_to_x(l):
-#     print(['X' if not (i+1) % 4 else num for i, num in enumerate(l)])
-#
+def change_to_x(l):
+    print(['X' if not (i+1) % 4 else num for i, num in enumerate(l)])
+
 # change_to_x(l)
 
 
 # 2) вивести на екран пустий квадрат з "*" сторона якого вказана як агрумент функції
 
-# def square(x):
-#     for i in range(x):
-#         if i == 0 or i == x-1:
-#             print('*'*x)
-#         else:
-#             print('*'+' '*(x-2)+'*')
-#
+def square(x):
+    for i in range(x):
+        if i == 0 or i == x-1:
+            print('*'*x)
+        else:
+            print('*'+' '*(x-2)+'*')
+
 # square(7)
 
 # 3) вывести табличку множення за допомогою цикла while
 
 
 
-# def calculator():
-#     i = 1
-#     size = 9
-#     while i <= 9:
-#         j = 1
-#         while j <= size:
-#             res = j * i
-#             print(' ' if res//10 else '  ', end='')
-#             print(res, end='')
-#             j += 1
-#         print()
-#         i += 1
-#
+def calculator():
+    i = 1
+    size = 9
+    while i <= 9:
+        j = 1
+        while j <= size:
+            res = j * i
+            print(' ' if res//10 else '  ', end='')
+            print(res, end='')
+            j += 1
+        print()
+        i += 1
+
 # calculator()
 
 
 # 4) переробити це завдання під меню
+while True:
+    menu = ['l = [22, 3,5,2,8,2,-23, 8,23,5]','1. Знайти мінімальне число в списку.', '2. Видалити всі дублікати значень.', '3. Замінити кожне четверте значення на "Х".', '4. Вивести квадрат із зірочок.', '5. Вивести табличку множення.', '9. Вихід', 'Виберіть пункт із меню: ']
 
-menu = ['l = [22, 3,5,2,8,2,-23, 8,23,5]','1. Знайти мінімальне число в списку.', '2. Видалити всі дублікати значень.', '3. Замінити кожне четверте значення на "Х".', '4. Вивести квадрат із зірочок.', '5. Вивести табличку множення.', 'Виберіть пункт із меню: ']
-
-menu_choice = input('\n'.join(menu))
-l = [22, 3,5,2,8,2,-23, 8,23,5]
-if menu_choice == '1':
-    def min_list_num(l):
-        print(min(l))
-    min_list_num(l)
-elif menu_choice == '2':
-    def set_list(l):
-        copy = l.copy()
-        print(set(copy))
-    set_list(l)
-elif menu_choice == '3':
-    def change_to_x(l):
-        copy = l.copy()
-        print(['X' if not (i+1) % 4 else num for i, num in enumerate(copy)])
-    change_to_x(l)
-elif menu_choice == '4':
-    def square(x):
-        for i in range(x):
-            if i == 0 or i == x-1:
-                print('*'*x)
-            else:
-                print('*'+' '*(x-2)+'*')
-    square(7)
-elif menu_choice == '5':
-    def calculator():
-        i = 1
-        size = 9
-        while i <= 9:
-            j = 1
-            while j <= size:
-                res = j * i
-                print(' ' if res//10 else '  ', end='')
-                print(res, end='')
-                j += 1
-            print()
-            i += 1
-
-    calculator()
+    menu_choice = input('\n'.join(menu))
+    if menu_choice == '1':
+        min_list_num(l)
+    elif menu_choice == '2':
+        set_list(l)
+    elif menu_choice == '3':
+        change_to_x(l)
+    elif menu_choice == '4':
+        square(7)
+    elif menu_choice == '5':
+        calculator()
+    else:
+        break
